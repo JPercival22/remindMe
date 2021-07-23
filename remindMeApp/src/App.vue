d2b3e0d36bb6e85ad327ea08281f80628091bc9d
Joseph Percival <joseph.m.percivaL@gmail.com>
2021-07-23 18:19:37 +0100

    initial commit

---
+++ remindMeApp/src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <div id="app"> 
+    <h1>remind<span class="blue-logo-text">Me</span></h1>
+    <h2>{{ msg }}</h2>
+    <h3>Essential Links</h3>
+    <toDo-List></toDo-List>
+  </div>
+</template>
+
+<script>
+import toDoList from './components/toDoList.vue'
+
+export default {
+  name: 'app',
+  components: {
+    toDoList
+  },
+  data () {
+    return {
+      msg: 'The App that reminds you to do your to-do list'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #fff;
+      display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid black;
+    border-radius: 3px;
+    max-width: 600px;
+    padding: 50px;
+}
+
+body {
+    margin: 0 auto;
+    max-width: 600px;
+    background-color: #dfdedeef;
+}
+h1, h2 {
+  color: black;
+  font-weight: 700;
+    .blue-logo-text{
+      color: dodgerblue;
+    }
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ remindMeApp/src/components/toDoList.vue
@@ -0,0 +1,71 @@
+<template>
+  <div>
+    <input
+      type="text"
+      class="todo-input"
+      placeholder="Type your to do task here ..."
+      v-model="newTodo" @keyup.enter="addTodo">
+    <div v-for="todo in todos" :key="todo.id" class="todo-item">
+      {{ todo.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "toDo-List",
+  data() {
+    return {
+      msg: "The App that reminds you to do your to-do list",
+
+      newTodo: "",
+      idForTodo: 3,
+      todos: [
+        {
+          id: 1,
+          title: "Finish Vue Project",
+          completed: "false",
+        },
+        {
+          id: 2,
+          title: "Hook in Laravel or Firebase on the back end for DB purposes",
+          completed: "false",
+        },
+      ],
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id:this.idForTodo,
+        title: this.newTodo,
+        completed: false,
+      })
+
+      this.newTodo = "",
+      this.idForTodo++
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.todo-input {
+  width: 100%;
+  padding: 10px 18px;
+  font-size: 18px;
+  margin-bottom: 16px;
+  &:focus {
+    outline: 0;
+  }
+}
+.todo-item::before {
+      content: "\2022";
+}
+.todo-item {
+    text-align: left;
+    font-size: 15px;
+    font-weight: 700;
+    padding: 10px 0;
+}
+</style>
